6b766003098308a6bada9b29a5823f3f51f21b49
Saba Abbasi <abbasisaba745@gmail.com>
2026-03-25 19:27:51 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,12 +16,17 @@ def greeting_chatbot(msg):
         
     elif any(word in msg for word in bye_message):
         return "Goodbye! See you next time."
+
+    elif any(word in msg for word in salam):
+        return "Wassalam"
         
     else:
         return "Sorry! i am just a greeting chatbot"
+        
 
 simple_message = ["hi","hello","hy","hey","hi there","hello there","hy there","hey there"]
 time_message = ["good morning", "good afternoon", "good evening", "good night"]
 conversation_message =["how are you", "what are you doing", "how is it going", "nice to meet you","what's up"]
 thanks_message = ['thanks','thank you','thanks a lot','thank you very much', 'thank you so much','thanks for everything']
 bye_message = ["bye", "by", "goodbye", "see you later", "see you soon", "take care", "exit"]
+salam = "assalamualaikum"
